fb3160665e4119478e5f91f57f11367203801d62
Matt Daniel <matt_daniel@intuit.com>
2016-08-02 17:12:21 -0700

    Beginning to check for major and changing the spec sheet based on major

--- src/components/Selection.js
+++ src/components/Selection.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Graph from './Graph';
-import {CSSpec} from '../utils/CSSpecv2';
 import {Constants} from '../utils/Constants';
+import {CSSpec} from '../utils/CSSpecv2';
 import {MESpec} from '../utils/MESpec';
 
 export default class Selection extends React.Component {
@@ -10,27 +10,42 @@ export default class Selection extends React.Component {
     constructor(props){
         super(props);
         this.state = {
-            show: true
+            show: true,
+            major: '',
+            track: ''
         }
     }
 
     handleClick = (e) => {
         e.preventDefault();
+        let major = this.state.major.trim().replace(/\s+/g, '_'),
+            track = this.state.track.trim().replace(/\s+/g, '_');
+            debugger;
+        let theSpec = Constants.spec.major;
+        debugger;
         this.setState({show: !this.state.show});
+    }
+
+    handleMajorChange = (e) => {
+        this.setState({major: e.target.value});
+    }
+
+    handleTrackChange = (e) => {
+        this.setState({track: e.target.value});
     }
 
     render(){
         if(this.state.show){
             return (
             <div>
-                <input type="text" placeholder="Major" />
-                <input type="text" placeholder="Track (optional)" />
-                <input type="submit" value="Post" onClick={this.handleClick} />
+                <input type="text" placeholder="Major" onChange={this.handleMajorChange} />
+                <input type="text" placeholder="Track (optional)" onChange={this.handleTrackChange} />
+                <input type="submit" value="Create Graph" onClick={this.handleClick} />
             </div>
             );
         }
         else{
-            return (<Graph spec={CSSpec.navSpecs} constants={Constants.constants} />);
+            return (<Graph spec={CSSpec.navSpecs} constants={Constants.webpage} />);
         }
     }
 }
--- src/utils/Constants.js
+++ src/utils/Constants.js
@@ -2,8 +2,12 @@ export const Constants = {
     "meta": {
         "name": "constants"
     },
-    "constants": {
+    "webpage": {
         "exploreCoursesFirst": "https://explorecourses.stanford.edu/search?view=catalog&filter-coursestatus-Active=on&page=0&catalog=&academicYear=&q=",
         "exploreCoursesSecond": "&collapse="
+    },
+    "spec": {
+        "Computer_Science": "CSSSpecv2",
+        "Mechanical_Engineering": "MESpec"
     }
 }
